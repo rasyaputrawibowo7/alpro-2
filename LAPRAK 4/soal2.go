a420de4d7eeec1d4deb23f5c8c0350fde55efff4
Rasya Putra Wibowo <rasyaputrawibowo62@gmail.com>
2026-04-06 22:47:52 +0700

    Add files via upload

---
+++ LAPRAK 4/soal2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hitungSkor(soal *int, skor *int) {
+	var waktu int
+	*soal = 0
+	*skor = 0
+
+	for i := 0; i < 8; i++ {
+		fmt.Scan(&waktu)
+		if waktu <= 300 {
+			*soal++
+			*skor += waktu
+		}
+	}
+}
+
+func main() {
+	var nama, pemenang string
+	var soal, skor int
+	var maxSoal, minSkor int
+
+	maxSoal = -1
+	minSkor = 1000000
+
+	for {
+		fmt.Scan(&nama)
+		if nama == "Selesai" {
+			break
+		}
+
+		hitungSkor(&soal, &skor)
+
+		if soal > maxSoal || (soal == maxSoal && skor < minSkor) {
+			maxSoal = soal
+			minSkor = skor
+			pemenang = nama
+		}
+	}
+
+	fmt.Println(pemenang, maxSoal, minSkor)
+}
